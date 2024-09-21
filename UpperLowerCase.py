da90fde52fdfda51f06ebb1d8ed8bcb6a3e24208
Rachana <152792371+Rachu9@users.noreply.github.com>
2024-09-21 22:33:47 +0530

    Add files via upload

---
+++ UpperLowerCase.py
@@ -0,0 +1,16 @@
+#Given a string you are requested to determine whether the string should be converted to all uppercae or all lowercase,depending on the count of uppercase and lowercase letter in that string.
+def uplo(inp):
+    l1=[]
+    l2=[]
+    for i in range(len(inp)):
+        if inp[i]>='A' and inp[i]<='Z':
+            l1.append(inp[i])
+        else:
+            l2.append(inp[i])
+    if len(l1)>len(l2):
+        a=inp.upper()
+    else:
+        a=inp.lower()
+    return a
+inp=input("Enter a String")
+print(uplo(inp))
